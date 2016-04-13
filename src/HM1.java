33e3c0a06284cc4503026c3cd7df6376b3d16859
oksanakonotop <oksanakonotop@ukr.net>
2016-04-13 20:47:32 +0300

    some bad commit with idea files

---
+++ src/HM1.java
@@ -0,0 +1,13 @@
+/**
+ * Created by oksanakonotop on 13.04.2016.
+ */
+public class HM1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Text git");
+    }
+
+}
+
+
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by oksanakonotop on 13.04.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int age = 26;
+        System.out.println(age);
+    }
+
+}
