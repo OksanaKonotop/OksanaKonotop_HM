1a0e5df3ab507807c03e40a3ed2f8bd6ef7dd866
oksanakonotop <oksanakonotop@ukr.net>
2016-04-22 16:43:25 +0300

    Копирование данных

---
+++ src/O1/Copy.java
@@ -0,0 +1,15 @@
+package O1;
+
+
+/**
+ * Created by oksanakonotop on 22.04.2016.
+ */
+public class Copy {
+    public static void main(String[] args) {
+
+        int[] a = {1,2,3};
+        int[] b = new int[a.length];
+        for (int i=0; i<a.length; i++)
+            b[i] = a[i];
+        }
+    }
